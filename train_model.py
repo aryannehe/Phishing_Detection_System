640f87b6698cae33e2a80913dcc604025c6b13f3
Aryan Nehe <aryannnehe@gmail.com>
2026-04-13 18:43:00 +0530

    Expand word TF-IDF vocabulary

--- train_model.py
+++ train_model.py
@@ -275,7 +275,7 @@ class CombinedFeatures(BaseEstimator, TransformerMixin):
         self.tfidf = TfidfVectorizer(
             analyzer='word',
             ngram_range=(1, 3),
-            max_features=8000,
+            max_features=10000,
             sublinear_tf=True,
             min_df=1,
         )
