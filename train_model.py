7fe34287e1958c62b6c3ef022095f519dc1bee33
Aryan Nehe <aryannnehe@gmail.com>
2026-04-18 09:43:00 +0530

    Adjust character n-gram range

--- train_model.py
+++ train_model.py
@@ -281,7 +281,7 @@ class CombinedFeatures(BaseEstimator, TransformerMixin):
         )
         self.tfidf_char = TfidfVectorizer(
             analyzer='char_wb',
-            ngram_range=(3, 5),
+            ngram_range=(2, 5),
             max_features=5000,
             sublinear_tf=False,
             min_df=3,
