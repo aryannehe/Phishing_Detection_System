3f622ec1e453c6df4bd6fea26d3bce62936acef4
Aryan Nehe <aryannnehe@gmail.com>
2026-05-08 18:43:00 +0530

    Restore sublinear TF scaling

--- train_model.py
+++ train_model.py
@@ -276,14 +276,14 @@ class CombinedFeatures(BaseEstimator, TransformerMixin):
             analyzer='word',
             ngram_range=(1, 4),
             max_features=8000,
-            sublinear_tf=False,
+            sublinear_tf=True,
             min_df=1,
         )
         self.tfidf_char = TfidfVectorizer(
             analyzer='char_wb',
             ngram_range=(2, 5),
             max_features=4000,
-            sublinear_tf=False,
+            sublinear_tf=True,
             min_df=1,
         )
         self.sec = SecurityFeatureExtractor()
