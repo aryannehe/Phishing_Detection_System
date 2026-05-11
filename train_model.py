be5fdf2f0579d91bdc91e4a913d96654cbaada7d
Aryan Nehe <aryannnehe@gmail.com>
2026-05-11 18:48:00 +0530

    Restore character n-gram range

--- train_model.py
+++ train_model.py
@@ -281,7 +281,7 @@ class CombinedFeatures(BaseEstimator, TransformerMixin):
         )
         self.tfidf_char = TfidfVectorizer(
             analyzer='char_wb',
-            ngram_range=(2, 5),
+            ngram_range=(3, 5),
             max_features=4000,
             sublinear_tf=True,
             min_df=1,
