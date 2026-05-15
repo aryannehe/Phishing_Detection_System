1779f1fc6244e2e821ad32c233016bcf88d83200
Aryan Nehe <aryannnehe@gmail.com>
2026-05-15 17:24:00 +0530

    Restore auto worker detection

--- train_model.py
+++ train_model.py
@@ -325,7 +325,7 @@ def train():
 
     print("Training ensemble model...")
     lr = LogisticRegression(C=5.0, max_iter=1000, class_weight='balanced', solver='lbfgs')
-    rf = RandomForestClassifier(n_estimators=200, max_depth=15, class_weight='balanced', random_state=42, n_jobs=4)
+    rf = RandomForestClassifier(n_estimators=200, max_depth=15, class_weight='balanced', random_state=42, n_jobs=-1)
     gb = GradientBoostingClassifier(n_estimators=150, max_depth=5, learning_rate=0.1, random_state=42)
 
     ensemble = VotingClassifier(
